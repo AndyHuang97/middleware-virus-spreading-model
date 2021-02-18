695f9bb164aeb17352412cdff0ce48a4705b3c7c
Nicolò Albergoni <albergoni.nicolo@gmail.com>
2021-02-18 17:42:14 +0100

    Merge parameters.h

--- src/parameters.h
+++ src/parameters.h
@@ -1,15 +1,15 @@
-#define POPULATION_SIZE 2000
-#define INITITAL_INFECTED 20
-#define GRID_HEIGHT 300
+#define POPULATION_SIZE 2
+#define INITITAL_INFECTED 1
+#define GRID_HEIGHT 500
 #define GRID_WIDTH 300
 #define TIME_STEP (60 * 1)
-#define END_TIME (60 * 60 * 24 * 30 * 4)
+#define END_TIME (60 * 4)
 #define SPEED 3
 #define SPREAD_DISTANCE 2
 #define VERBOSE false
 #define SUSCEPTIBILITY_THR (60 * 60 * 24 * 90)
 #define INFECTION_THR (60 * 10)
 #define IMMUNITY_THR (60 * 60 * 24 * 10)
-#define COUNTRY_HEIGHT 50
-#define COUNTRY_WIDTH 50
+#define COUNTRY_HEIGHT 100
+#define COUNTRY_WIDTH 100
 #define DAY (60 * 60 * 24)
